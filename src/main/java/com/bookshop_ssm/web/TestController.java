cfdb4433e088c70c95a26e51d9b4e027de1f27f7
lin <904446806@qq.com>
2019-01-02 22:55:47 +0800

    添加登录功能

--- src/main/java/com/bookshop_ssm/web/TestController.java
+++ src/main/java/com/bookshop_ssm/web/TestController.java
@@ -2,49 +2,50 @@ package com.bookshop_ssm.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import javax.servlet.http.HttpSession;
 
 @Controller
 public class TestController {
-    //访问index测试session登陆权限
-    @RequestMapping(value = "/admintt",method = RequestMethod.GET)
-    public String select(Model model,HttpSession session)
+
+    //登录页
+    @RequestMapping("/login")
+    public String login(){
+        return "/pages/examples/login";
+    }
+
+    //登录验证
+    @RequestMapping("/loginaction")
+    public String select(Model model, HttpSession session, @RequestParam("email") String email, @RequestParam("password") String password)
     {
         String user = (String)session.getAttribute("user");
-        if (user.equals(null)){
-            return  "/pages/examples/login";
+        if (user==null){
+            if (email.equals(password)){
+                session.setAttribute("user",email); //设置session
+                return "redirect:admin";
+            }else{
+                model.addAttribute("message","密码错误，请重新输入！");
+                return  "/pages/examples/login";
+            }
         }else{
             model.addAttribute("user",user);
             return "index";
         }
 
     }
-    //登陆页面
-    @RequestMapping(value = "/login",method = RequestMethod.GET)
-    public String login(Model model)
+    //等入主界面
+    @RequestMapping(value = "/admin")
+    public String admin(Model model,HttpSession session)
     {
-        /*model.addAttribute("username","Hello world!!!");*/
-        return "/pages/examples/login";
+        String user = (String)session.getAttribute("user");
+        model.addAttribute("user",user);
+        return "index";
     }
-    //登陆验证
-    @RequestMapping("/admin")
-    public String loginaction(HttpSession session,Model model,@RequestParam("email") String email, @RequestParam("password") String password){
-        if (email.equals(password)){
-            session.setAttribute("user",email); //设置session
-            model.addAttribute("user",email); //返回值给模板
-            return "index";
-        }else{
-            model.addAttribute("message","用户名或者密码错误，请重新登陆！");
-            return  "/pages/examples/login";
-        }
-    }
+
     //登出
     @RequestMapping("/signout")
     public String signout(HttpSession session){
         session.removeAttribute("user");
-        return "login";
+        return "/pages/examples/login";
     }
 }
--- src/main/webapp/WEB-INF/AdminLTE/pages/examples/login.ftl
+++ src/main/webapp/WEB-INF/AdminLTE/pages/examples/login.ftl
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | Log in</title>
+  <title>AdminLTE 2 | Login</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
@@ -30,13 +30,13 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="login"><b>后台管理</b></a>
+    <a href="admin"><b>后台管理</b></a>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
-    <p class="login-box-msg">${message!'Sign in to start your session'}</p>
+    <p class="login-box-msg">${message!'输入密码以登录！'}</p>
 
-    <form action="../../loginaction" method="post">
+    <form action="loginaction" method="post">
       <div class="form-group has-feedback">
         <input name="email" type="email" class="form-control" placeholder="Email">
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
@@ -49,29 +49,29 @@
         <div class="col-xs-8">
           <div class="checkbox icheck">
             <label>
-              <input type="checkbox"> Remember Me
+              <input type="checkbox"> 记住我
             </label>
           </div>
         </div>
         <!-- /.col -->
         <div class="col-xs-4">
-          <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+          <button type="submit" class="btn btn-primary btn-block btn-flat">登录</button>
         </div>
         <!-- /.col -->
       </div>
     </form>
 
-    <div class="social-auth-links text-center">
+    <#--<div class="social-auth-links text-center">
       <p>- OR -</p>
       <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
         Facebook</a>
       <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
         Google+</a>
-    </div>
+    </div>-->
     <!-- /.social-auth-links -->
 
-    <a href="#">I forgot my password</a><br>
-    <a href="register.html" class="text-center">Register a new membership</a>
+    <a href="#">忘记密码了</a><br>
+    <a href="register.html" class="text-center">注册新用户</a>
 
   </div>
   <!-- /.login-box-body -->
